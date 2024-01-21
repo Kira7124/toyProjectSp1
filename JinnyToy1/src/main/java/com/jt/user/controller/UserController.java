7a4404d8e48bf1c410c9f67a91d04f337a66bb6c
Kira7124 <afc2015@naver.com>
2024-01-21 16:54:26 +0900

    db연결안되서 안뜨는것 까지는 해결! 요구사항정의서 정리중....

--- JinnyToy1/src/main/java/com/jt/user/controller/UserController.java
+++ JinnyToy1/src/main/java/com/jt/user/controller/UserController.java
@@ -4,13 +4,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/user/*")
 @Controller
+@RequestMapping("/user/*")
 public class UserController {
 	
 	
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	
+	// 메인페이지 들어가기
+	// http://localhost:8088/user/mainpage
+	
+	
+	
+	
+	//메인페이지 호출 GET
+	@RequestMapping(value="/mainpage",method=RequestMethod.GET)
+	public void mainpage() {
+		logger.debug("main 페이지 출력!");
+		
+	}
+	
+	
+	
+	
+	
 	
 	
 	
