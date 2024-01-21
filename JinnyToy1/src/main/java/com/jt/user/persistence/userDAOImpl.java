9ba3cb739eeeebcc7289b8f6510dc79b4e87aa6d
Kira7124 <afc2015@naver.com>
2024-01-22 08:30:32 +0900

    1/22 세팅완

--- JinnyToy1/src/main/java/com/jt/user/persistence/userDAOImpl.java
+++ JinnyToy1/src/main/java/com/jt/user/persistence/userDAOImpl.java
@@ -1,6 +1,31 @@
 package com.jt.user.persistence;
 
+import javax.inject.Inject;
 
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class userDAOImpl implements userDAO {
 
+	
+	private static final Logger logger = LoggerFactory.getLogger(userDAOImpl.class);
+	
+	
+	// DB 에 접근할 객체 주입
+	@Inject
+	SqlSession sqlSession;
+	
+	private final String NAMESPACE="com.jt.mapper.UserMapper";
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- JinnyToy1/src/main/java/com/jt/user/service/userServiceImpl.java
+++ JinnyToy1/src/main/java/com/jt/user/service/userServiceImpl.java
@@ -1,8 +1,10 @@
 package com.jt.user.service;
 
+import org.springframework.stereotype.Repository;
+
 import com.jt.user.persistence.userDAO;
 
-
+@Repository
 public class userServiceImpl implements userDAO {
 	
 }
